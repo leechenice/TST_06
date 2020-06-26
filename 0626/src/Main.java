002615a9dccc52c07c9e4b626f7a59fe63ab5832
leechenice <leechenice@gmail.com>
2020-06-26 23:08:28 +0800

    :>

---
+++ 0626/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1*1.0);
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int a1 = 0,a2 = 0,a3 = 0,a5 = 0,p = 1,q = 0;
+//        float a4 = 0;
+//        while (n-- > 0) {
+//            int k = in.nextInt();
+//            switch(k % 5) {
+//                case 0 :
+//                    if (k % 2 == 0) a1 += k;break;
+//                case 1 :
+//                    a2 += k*p; p = -p;break;
+//                case 2 :
+//                    a3++;break;
+//                case 3 :
+//                    a4 += k;q++;break;
+//                case 4 :
+//                    a5 = Math.max(a5, k);break;
+//            }
+//        }
+//        if (a1 == 0) System.out.print("N ");
+//        else System.out.print(a1);
+//        if (a2 == 0) System.out.print("N ");
+//        else System.out.print(a2);
+//        if (a3 == 0) System.out.print("N ");
+//        else System.out.print(a3);
+//        if (a4 == 0) System.out.print("N ");
+//        else System.out.print(a4/q);
+//        if (a5 == 0) System.out.print("N");
+//        else System.out.print(a5);
+
+    }
+}
